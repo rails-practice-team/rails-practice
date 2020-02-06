f1b6c8b860b211c7b5de7d812077ff9f6f1d5f7c
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-02-06 15:31:02 +0900

    modified view-pages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,8 +7,13 @@ class TweetsController < ApplicationController
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user_id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 end
