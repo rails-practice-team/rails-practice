62c9a8e472e7af8b8d1a493aeb45c8bff8b1e75c
Keko-hagari <59214361+Keko-hagari@users.noreply.github.com>
2020-03-08 17:28:17 +0900

    comments_controller edit,destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,30 @@ class CommentsController < ApplicationController
             render 'tweets/show'
         end
     end
+    def edit
+        @comment = Comment.find(params[:id])
+        if  @comment.user_id != current_user.id
+            redirect_to tweet_path(@comment.tweet_id.to_i)
+        end
+    end
+    def update
+        @comment = Comment.find(params[:id])
+        if @comment.update(comment_params)
+            redirect_to tweet_path(@comment.tweet_id.to_i)
+        else
+            @comment = Comment.find(params[:id])
+            render :edit
+        end
+    end
     def destroy
+        @comment = Comment.find(params[:id])
+        number = @comment.tweet_id
+        if @comment.destroy
+          redirect_to tweet_path(number)
+        else
+          @comment = Comment.find(params[:id])
+          render :edit
+        end
     end
     private
     def comment_params
